e7bb71cda1dc607fa3f60e3be292b6da9df01e1c
hunyoung529 <hunyoung529@gmail.com>
2023-11-15 17:39:44 +0900

    z

--- next.config.js
+++ next.config.js
@@ -1,8 +1,38 @@
+/** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa')
+// const withPlugins = require("next-compose-plugins");
+// const nextConfig = {
+//     reactStrictMode: false,
+// }
+// module.exports = withPlugins(
+// 	[
+// 		[
+// 			withPWA,
+// 			{
+// 				pwa: {
+// 					dest: "public",
+// 				},
+// 			},
+// 		],
+// 		// 추가 플러그인 작성
+// 	],
+// 	nextConfig
+// );
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  // next.js config
+});
+
+/* 
+
 const path = require("path");
 const withPWAInit = require("next-pwa");
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack: (config) => {
     const entry = generateAppDirEntry(config.entry);
     config.entry = () => entry;
@@ -11,28 +41,68 @@ const nextConfig = {
   },
 };
 
+
 const withPWA = withPWAInit({
-  dest: "public",
-  buildExcludes: ["manifest.json"],
+dest: "public",
+buildExcludes: ["app-build-manifest.json"],
 });
 
 const generateAppDirEntry = (entry) => {
-  const packagePath = require.resolve("next-pwa");
-  const packageDirectory = path.dirname(packagePath);
-  const registerJs = path.join(packageDirectory, "register.js");
+const packagePath = require.resolve('next-pwa');
+const packageDirectory = path.dirname(packagePath);
+const registerJs = path.join(packageDirectory, "register.js");
 
-  return entry().then((entries) => {
-    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
-      if (Array.isArray(entries["main-app"])) {
-        entries["main-app"].unshift(registerJs);
-      } else if (typeof entries["main-app"] === "string") {
-        entries["main-app"] = [registerJs, entries["main-app"]];
-      }
+return entry().then((entries) => {
+  if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+    if (Array.isArray(entries["main-app"])) {
+      entries["main-app"].unshift(registerJs);
+    } else if (typeof entries["main-app"] === "string") {
+      entries["main-app"] = [registerJs, entries["main-app"]];
     }
-    return entries;
-  });
+  }
+  return entries;
+});
 };
 
-if (process.env.NEXT_PUBLIC_MODE != "development") {
-  module.exports = withPWA(nextConfig);
-}
+
+
+module.exports = withPWA(nextConfig); */
+
+// const path = require("path");
+// const withPWAInit = require("next-pwa");
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   webpack: (config) => {
+//     const entry = generateAppDirEntry(config.entry);
+//     config.entry = () => entry;
+
+//     return config;
+//   },
+// };
+
+// const withPWA = withPWAInit({
+//   dest: "public",
+//   buildExcludes: ["manifest.json"],
+// });
+
+// const generateAppDirEntry = (entry) => {
+//   const packagePath = require.resolve("next-pwa");
+//   const packageDirectory = path.dirname(packagePath);
+//   const registerJs = path.join(packageDirectory, "register.js");
+
+//   return entry().then((entries) => {
+//     if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+//       if (Array.isArray(entries["main-app"])) {
+//         entries["main-app"].unshift(registerJs);
+//       } else if (typeof entries["main-app"] === "string") {
+//         entries["main-app"] = [registerJs, entries["main-app"]];
+//       }
+//     }
+//     return entries;
+//   });
+// };
+
+// if (process.env.NEXT_PUBLIC_MODE != "development") {
+//   module.exports = withPWA(nextConfig);
+// }
